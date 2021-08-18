fb396941c0ee0ef9e88cd043435b48ec88350b55
Surya Rajesh <89063905+SuryaRajesh02@users.noreply.github.com>
2021-08-18 18:27:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function makePink(){
+    document.body.style.backgroundColor="pink";
+}
+
+function makeBlue(){
+    document.body.style.backgroundColor="blue";  
+}
+
+function makeRed(){
+    document.body.style.backgroundColor="red";  
+}
+
+function makeYellow(){
+    document.body.style.backgroundColor="yellow";  
+}
